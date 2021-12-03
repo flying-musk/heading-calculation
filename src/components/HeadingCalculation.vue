541168177e3169eb2335dc0d469eecdbd0edb9da
Flying Musk <musk.flying@gmail.com>
2021-12-03 15:59:19 +0800

    [heading-calculation] Init project setup.

---
+++ src/components/HeadingCalculation.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="h-outer">Hi heading calculation</div>
+</template>
+
+<script>
+export default {
+  name: 'HeadingCalculation',
+};
+</script>
+
+<style scoped lang="scss">
+.h-outer {
+  border: solid 1px greenyellow;
+}
+</style>
