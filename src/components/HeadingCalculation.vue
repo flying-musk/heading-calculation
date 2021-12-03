757d6b5ccebd066306b67949d24dbba5290d176a
Flying Musk <musk.flying@gmail.com>
2021-12-03 18:52:12 +0800

    [heading-calculation] Implement update functions for heading, direction, difference.

--- src/components/HeadingCalculation.vue
+++ src/components/HeadingCalculation.vue
@@ -4,12 +4,13 @@
     <p>Click on the question or press ENTER to get answer.</p>
     <p>Click on NEW or press RIGHT to get new questions.</p>
     <div class="h-main" v-if="!showAnswer" @click="toggleClick">
-      <div class="main-content">72</div>
-      <div class="main-content">Left turn</div>
-      <div class="main-content">330</div>
+      <div class="main-content">{{ currentHeading }}</div>
+      <div class="main-content" v-if="isLeft">Left turn</div>
+      <div class="main-content" v-if="!isLeft">Right turn</div>
+      <div class="main-content">{{ degreeDifference }}</div>
     </div>
     <div class="h-main" v-if="showAnswer" @click="toggleClick">
-      <div class="main-content">102</div>
+      <div class="main-content">{{ answer }}</div>
     </div>
     <button class="main-button" @click="nextQuestion">NEW</button>
   </div>
@@ -19,14 +20,45 @@
 export default {
   name: 'HeadingCalculation',
   data() {
-    return { showAnswer: false };
+    return {
+      updateFlag: 0,
+      showAnswer: false,
+      currentHeading: 0,
+      isLeft: false,
+      degreeDifference: 0,
+      answer: 0,
+    };
+  },
+  watch: {
+    updateFlag: {
+      immediate: true,
+      handler() {
+        this.currentHeading = this.newCurrentHeading();
+        this.isLeft = this.newIsLeft();
+        this.degreeDifference = this.newDegreeDifference();
+        this.answer = this.newAnswer();
+      },
+    },
   },
   methods: {
     nextQuestion() {
       this.showAnswer = false;
+      this.updateFlag += 1;
     },
     toggleClick() {
       this.showAnswer = !this.showAnswer;
+    },
+    newCurrentHeading() {
+      return Math.trunc(Math.random() * 3600000) % 360;
+    },
+    newIsLeft() {
+      return Math.random() < 0.6;
+    },
+    newDegreeDifference() {
+      return (Math.trunc((Math.random() * 100000) / 5) * 5) % 1080;
+    },
+    newAnswer() {
+      return 100;
     },
   },
   created() {
