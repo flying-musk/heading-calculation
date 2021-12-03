3f1675b35280475c9d75f039d9fe60d39608d5eb
Flying Musk <musk.flying@gmail.com>
2021-12-03 17:18:22 +0800

    [heading-calculation] Finish main layout of component: HeadingCalculation.

--- src/components/HeadingCalculation.vue
+++ src/components/HeadingCalculation.vue
@@ -1,5 +1,20 @@
 <template>
-  <div class="h-outer">Hi heading calculation</div>
+  <div id="heading_calculation" class="h-outer">
+    <h1>Heading calculation</h1>
+    <p>Click on the question or press ENTER to get answer.</p>
+    <p>Click on NEW or press RIGHT to get new questions.</p>
+    <div class="h-main">
+      <div class="main-content">72</div>
+      <div class="main-content">Left turn</div>
+      <div class="main-content">330</div>
+    </div>
+    <!--
+    <div class="h-main">
+      <div class="main-content">102</div>
+    </div>
+    -->
+    <button class="main-button">NEW</button>
+  </div>
 </template>
 
 <script>
@@ -10,6 +25,31 @@ export default {
 
 <style scoped lang="scss">
 .h-outer {
-  border: solid 1px greenyellow;
+  display: grid;
+}
+.h-main {
+  cursor: pointer;
+  border: solid 1px cornflowerblue;
+  min-height: 50vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.main-content {
+  font-size: 108px;
+}
+.main-button {
+  justify-self: center;
+  padding: 48px;
+  font-size: 52px;
+}
+@media (max-width: 600px) {
+  .h-main {
+    min-height: 24vh;
+  }
+  .main-content {
+    font-size: 36px;
+  }
 }
 </style>
